7ff6d1fd6f022598cf76c8769b8b0e60d8a76a07
st9540808 <st9540808@gmail.com>
2017-06-24 17:31:43 +0800

    fix indention in insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -11,8 +11,8 @@ SinglyLList::ListNode* SinglyLList::insertionSort() // stable sort
 		nextPtr = currentPtr->next;
 		if (nextPtr->val < currentPtr->val) {
 			for (tempPtr = &head
-					; *tempPtr != nextPtr and (*tempPtr)->val <= nextPtr->val
-					; tempPtr = &(*tempPtr)->next) ;
+			        ; *tempPtr != nextPtr and (*tempPtr)->val <= nextPtr->val
+			        ; tempPtr = &(*tempPtr)->next) ;
 			currentPtr->next = nextPtr->next;
 			nextPtr->next = *tempPtr;
 			*tempPtr = nextPtr;
@@ -39,9 +39,9 @@ SinglyLList::ListNode* SinglyLList::insertionSort_old() // stable sort
 				nextPtr->next = head;
 				head = nextPtr;
 			} else {
-				for (tempPtr = head;
-				        tempPtr->next != currentPtr and tempPtr->next->val <= nextPtr->val;
-				        tempPtr = tempPtr->next) ;
+				for (tempPtr = head
+				        ; tempPtr->next != currentPtr and tempPtr->next->val <= nextPtr->val
+				        ; tempPtr = tempPtr->next) ;
 				currentPtr->next = nextPtr->next;
 				nextPtr->next = tempPtr->next;
 				tempPtr->next = nextPtr;
